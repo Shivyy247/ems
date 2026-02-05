c8d991ff134337b565b409cf5874ed3839a5cd63
Shivani Barman <shivanibarman247@gmail.com>
2026-02-05 20:52:46 +0530

    refactor authentication flow; implement context for user data and local storage management

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
-import React, { useEffect } from 'react'
+import React, {useContext, useEffect } from 'react'
 import Login from './components/Auth/Login';
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard';
 import AdminDashboard from './components/Dashboard/AdminDashboard';
 import { getLocalStorage, setLocalStorage } from './utils/localStorage';
 import { useState } from 'react';
+import { AuthContext } from './context/AuthProvider';
 
 
 const App = () => {
 
   const [user, setUser] = useState(null)
+  const authData = useContext(AuthContext);
+  
+  useEffect(() => {
+    if (authData) {
+      const loggedInUser = localStorage.getItem("loggedInUser")
+      if (loggedInUser)
+      {
+        setUser(loggedInUser.role)
+      }
+    }
+  },[authData])
 
-  const handleLogin = (email,password) => {
+  const handleLogin = (email, password) => {
+    
     if (email == 'admin@me.com' && password == '123') {
       setUser('admin')
+      localStorage.setItem('loggedInUser',JSON.stringify({role:'admin'}))
     }
-    else if (email == "user@me.com" && password == "123") {
+    else if (authData && authData.employees.find((e)=>email == e.email && e.password == password)) {
       setUser('employee')
+      localStorage.setItem("loggedInUser", JSON.stringify({ role: "employee" }));
     } else {
       alert("Invalid credentials");
     }
   }
+
+  
 
 
   useEffect(() => {

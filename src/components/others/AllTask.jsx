5c2a05816e18332ae119b2339198ec627f84b788
Shivani Barman <shivanibarman247@gmail.com>
2026-02-06 22:56:04 +0530

    refactor AllTask component; integrate AuthContext for dynamic employee task rendering

--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {AuthContext} from '../../context/AuthProvider'
 
 const AllTask = () => {
 
+  const authData = useContext(AuthContext);
   
+
   return (
-    <div className="bg-[#1c1c1c] p-5  rounded mt-5  h-60 overflow-auto">
-      <div className="bg-blue-500 mb-2  py-2 px-4 flex justify-between rounded ">
-        <h2>Shivi</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
+    <div className="bg-[#1c1c1c] p-5  rounded mt-5 overflow-auto">
+      <div className="bg-blue-950 mb-2  py-2 px-4 flex justify-between rounded ">
+        <h2 className="text-lg font-bold w-1/5">Employee Name</h2>
+        <h3 className="text-lg font-bold w-1/5"> New Task</h3>
+        <h5 className="text-lg font-bold w-1/5">Active </h5>
+        <h5 className="text-lg font-bold w-1/5">Completed</h5>
+        <h5 className="text-lg font-bold w-1/5">Failed</h5>
       </div>
-      <div className="bg-green-500 mb-2  py-2 px-4 flex justify-between rounded ">
-        <h2>Shivi</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-yellow-500 mb-2  py-2 px-4 flex justify-between rounded ">
-        <h2>Shivi</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-purple-500 mb-2  py-2 px-4 flex justify-between rounded ">
-        <h2>Shivi</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-emerald-500 mb-2  py-2 px-4 flex justify-between rounded ">
-        <h2>Shivi</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
+
+      <div className=' overflow-auto'>
+        {authData.employees.map(function (elem) {
+          return (
+            <div className="bg-blue-500 mb-2  py-2 px-4 flex justify-between rounded ">
+              <h2 className="text-lg font-semibold w-1/5 text-blue-950">{elem.firstname}</h2>
+              <h3 className="text-lg font-semibold w-1/5 text-blue-800">{elem.taskNumber.newTask} </h3>
+              <h5 className="text-lg font-semibold w-1/5 text-blue-800">{elem.taskNumber.active}</h5>
+              <h5 className="text-lg font-semibold w-1/5 text-blue-800">{elem.taskNumber.completed}</h5>
+              <h5 className="text-lg font-semibold w-1/5 text-blue-800">{elem.taskNumber.failed}</h5>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
